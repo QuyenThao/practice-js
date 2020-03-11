9dbffccdf41a5b14577bb4285fb905d9d8dd8c8d
QuyenThao <khanhvuong123s@gmail.com>
2020-03-11 12:10:31 +0700

    ex12

---
+++ ex12.js
@@ -0,0 +1,22 @@
+// Write a JavaScript program to sort an array of all prime numbers between 1 and a given integer.
+const readlineSync = require('readline-sync');
+
+function primeNum() {
+    console.log('Array of all prime numbers between 1 and n ');
+    let n =  readlineSync.question('n = ');
+    let arr = [];
+    let arr1 = [];
+    for (var i = 0; i <= n; i++) {
+        arr1.push(true);
+    }
+    for (var i = 2; i <= n; i++) {
+        if (arr1[i]) {
+            arr.push(i);
+            for (let j = 1; i * j <= n; j++) {
+            arr1[i * j] = false;
+            }
+        }
+    }    
+    return arr;
+}
+console.log(primeNum());

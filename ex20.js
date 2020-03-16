ee34f62401e3a6de761112dd7c131eb733a72ef8
QuyenThao <thaoquyen23012003@gmail.com>
2020-03-16 10:17:20 +0700

    hdhg

---
+++ ex20.js
@@ -0,0 +1,13 @@
+// Write a JavaScript program to check whether a given array of integers represents either a strictly increasing or a strictly decreasing sequence.
+// Return true if it increasing & return false if it decreasing
+function is_monotonous(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[i + 1]) {
+            return true;
+        } else {
+            return false;  
+    }
+}
+console.log(is_monotonous([10, 6, -2]));
+console.log(is_monotonous([1, -6, 9]));
+console.log(is_monotonous([-5, -3, 0]));
